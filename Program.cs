78a0feb70aa128016a637f31e604c7e790eefa8f
one3816 <one3816@mail.ru>
2023-11-28 19:11:33 +0500

    git init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int count = 0;
+int[] array = new int[10];
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 101);
+    Console.Write(array[i] + " ");
+}
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] >= 20 && array[i] <= 100)
+    {
+        count++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine(count);
